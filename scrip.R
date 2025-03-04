bad092ccc50ed2947dfd214ad50cd3438fa0d03f
Shizen98 <e.juliatorres@go.ugr.es>
2025-03-04 18:56:29 +0100

    añado mi opinión sobre R

--- scrip.R
+++ scrip.R
@@ -1,1 +1,3 @@
-Este es un scrip para la actividad 1 de control de versiones de Github
+# Este es un scrip para la actividad 1 de control de versiones de Github
+
+# Cada vez me gusta más trabajar con R
