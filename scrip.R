945178cc45bdb7888f8c2fb78f8abea6799702fb
Shizen98 <e.juliatorres@go.ugr.es>
2025-03-04 18:57:20 +0100

    otro commit de declaración de intenciones

--- scrip.R
+++ scrip.R
@@ -1,3 +1,5 @@
 # Este es un scrip para la actividad 1 de control de versiones de Github
 
 # Cada vez me gusta más trabajar con R
+
+# voy a probar a hacer muchos commit sin hacer push para ver como se ve luego en Github
